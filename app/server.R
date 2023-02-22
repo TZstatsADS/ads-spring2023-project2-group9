4ed4bc0a8e746dd5783f5311a5cff7ec1023e9af
Xinming <471836430@qq.com>
2023-02-22 00:59:10 -0500

    1

--- app/server.R
+++ app/server.R
@@ -7,198 +7,155 @@
 #    http://shiny.rstudio.com/
 #
 ###############################Install Related Packages #######################
-if (!require("shiny")) {
-    install.packages("shiny")
-    library(shiny)
-}
-if (!require("leaflet")) {
-    install.packages("leaflet")
-    library(leaflet)
-}
-if (!require("leaflet.extras")) {
-    install.packages("leaflet.extras")
-    library(leaflet.extras)
-}
-if (!require("dplyr")) {
-    install.packages("dplyr")
-    library(dplyr)
-}
-if (!require("magrittr")) {
-    install.packages("magrittr")
-    library(magrittr)
-}
-if (!require("mapview")) {
-    install.packages("mapview")
-    library(mapview)
-}
-if (!require("leafsync")) {
-    install.packages("leafsync")
-    library(leafsync)
-}
-
-#Data Processing
-total_citi_bike_df = read.csv('../data/citibike_data.csv')
-##compute the daily in and out difference for the station
-total_citi_bike_df$day_diff = total_citi_bike_df$endcount - total_citi_bike_df$startcount
-#assign each column to weekend or weekday
-total_citi_bike_df$weekend_or_weekday = ifelse(total_citi_bike_df$weekday %in% c('Saturday','Sunday'), "Weekend", "Weekday")
-
-#station info
-citi_bike_station_info <- total_citi_bike_df[,c('station_id','station_name','station_longitude','station_latitude')]
-#remove the duplicates based on station id 
-citi_bike_station_info <- citi_bike_station_info[!duplicated(citi_bike_station_info[ , c("station_id")]),]
-
-#split the bike data to pre-covid and covid time period
-citi_bike_pre_covid_df = total_citi_bike_df[difftime(total_citi_bike_df$date,"2019-05-31")<=0,] #2019-05-01 ~ 2019-05-31
-citi_bike_covid_df = total_citi_bike_df[difftime(total_citi_bike_df$date,"2020-04-30")>=0,] #2020-05-01 ~ 2021-05-31
+library(shiny)
+library(dplyr)
+library(tidyr)
+library(scales)
+library(ggplot2)
+library(readr)
+library(sp)
+library(rgdal)
+library(leaflet)
 
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+df_counts <- read_csv('df_counts.csv')
+df_res <- read_csv('df_res.csv')
+df_vio <- read.csv('Processed_Violence.csv', header=T)
+################################################
+################################################
+grade_rent <- read.csv("grade_rent.csv")
+coordinates(grade_rent) = c("lng","lat")
+crs.geo1 = CRS("+proj=longlat")  
+proj4string(grade_rent) = crs.geo1 
+newyork <- readOGR('nyu_2451_34509.shp')
+proj4string(newyork) = crs.geo1
+nyc_agg = aggregate(x=grade_rent["avg_rent"],by=newyork,FUN=function(x) round(mean(x), 2))
+nyc_agg2 = aggregate(x=grade_rent["tot_A"],by=newyork,FUN=function(x) sum(x))
+bins <- c(0, 1500, 2500, 3000, 3500, 4100)
+qpal = colorBin("YlGnBu", nyc_agg$avg_rent, bins=bins,na.color = "transparent")
+labels <- sprintf(
+  ifelse(!is.na(nyc_agg$avg_rent),
+         "<strong>Price: $%s<br>Grade A Restaurants: %s</strong>",
+         ""),
+  nyc_agg$avg_rent,
+  nyc_agg2$tot_A
+) %>% lapply(htmltools::HTML)
+##########################################################
+##########################################################
 
-    ## Map Tab section
+
+colnames(df_vio) <- c("Boro", "Year", "Family Felony Assaults", "Domestic Violence Felony Assaults", "Domestic Violence Incident", "Rape")
+###traffic
+traffic <- read.csv("morningtraffic.csv")
+colnames(traffic) <- c("time", "Manhattan", "Brooklyn", "Bronx", "Queens", "Staten Island")
+traffic2 <- read.csv("eveningtraffic.csv")
+colnames(traffic2) <- c("time", "Manhattan", "Brooklyn", "Bronx", "Queens", "Staten Island")
+
+### colors
+color <- c("cornflowerblue", 'firebrick2', "coral", "blue", "yellow", "pink","green", "white","black",
+                           "grey", "orange", "violet","blueviolet","darkseagreen1","burlywood1")
+                           
+
+ui = navbarPage(
+  title = "Project2_group9",
+  tabPanel(title = "Intro", includeMarkdown("Introduction.Rmd")),
+  tabPanel(title = "Violence",
+           titlePanel(title = "New York Violence Information"),
+           sidebarLayout(
+             position = "right",
+             sidebarPanel(
+               selectInput(inputId = "area", label = "Area:", choices = c("Manhattan", "Brooklyn", "Bronx", "Queens", "Staten Island"))
+             ),
+             mainPanel(plotOutput("hist"))
+           )
+           
+  ),
+  tabPanel(title = "Traffic",
+           titlePanel(title = "New York Traffic Information"),
+           sidebarLayout(
+             sidebarPanel(
+               selectInput(inputId = "loc", label = "Boro:", choices = colnames(traffic)[-1]),
+               selectInput(inputId = "df", label = "Time:", choices = c("Morning", "Evening")),
+               selectInput(inputId = "col", label = "Color:", choices = color)
+             ),
+             mainPanel(plotOutput("scatter_plot"))
+           )),
+  tabPanel(
+    title = "Restaurant",
+    titlePanel(title = "New York Resturant infomation"),
+    sidebarLayout(
+      sidebarPanel(
+        "Pick Any Color You Like.",
+        selectInput(inputId = "col1", label = "Other:", choices = sample(color,length(color),replace = FALSE)),
+        selectInput(inputId = "col2", label = "A:", choices = sample(color,length(color),replace = FALSE)),
+        selectInput(inputId = "col3", label = "B:", choices = sample(color,length(color),replace = FALSE)),
+        selectInput(inputId = "col4", label = "C:", choices = sample(color,length(color),replace = FALSE)),
+      ),
+      mainPanel(plotOutput("plot"))
+    )),
+  tabPanel(title = "Map",
+           titlePanel(title = "Map Information"),
+           mainPanel(leafletOutput("map"))),
+  tabPanel(title = "Reference", includeMarkdown("Reference.Rmd"))
+)
+
+
+server = function(input, output) {
+  
+  output$map <- renderLeaflet({
     
-    output$left_map <- renderLeaflet({
+    leaflet(nyc_agg) %>%
+      addProviderTiles("CartoDB") %>% 
+      addPolygons(stroke = TRUE,opacity = 1,fillOpacity = 0.5, smoothFactor = 0.5,
+                  color="white",fillColor = ~qpal(avg_rent),weight = 1, label = labels,
+                  labelOptions = labelOptions(
+                    style = list("font-weight" = "normal", padding = "3px 8px"),
+                    textsize = "15px",
+                    direction = "auto")) %>%
+      addLegend(values=~avg_rent,pal=qpal,title="Rental price")
+  })
+  
+  
+  output$scatter_plot <- renderPlot({
+    if(input$df== "Morning"){df = traffic}
+    else{df = traffic2}
+    plot(as.POSIXct(df$time), df[input$loc][,1], type="o", col=input$col, 
+         pch="o", ylab="Counts",xlab='Month', lwd=3,lty=1,main= paste0(input$df, " Daily Traffic Volume Counts in ", input$loc))
+  })
+  
+  output$plot <- renderPlot({
+    ggplot() + 
+      geom_col(aes(x = as.factor(BORO), y = 1, fill = grade), data = df_res, group = 1, position = "stack")+ 
+      geom_line(aes(x = as.factor(df_counts$Borough), y = 39000*df_counts$Ratios, group = 1), color = "red",lwd=1)+ 
+      geom_point(aes(x = as.factor(df_counts$Borough), y = 39000*df_counts$Ratios), color = "black") +
+      scale_fill_manual(values=c("OTHER"=input$col1, "A"=input$col2, "B"=input$col3, "C"=input$col4)) +
+      labs(title= "Restaurants Grade Distribution", x = "Borough", y = "Counts") +
+      guides(fill=guide_legend(title="GRADE")) +
+      scale_y_continuous(sec.axis=sec_axis(~.*1/400, name="Ratios of Grade A")) +
+      theme(plot.title = element_text(hjust = 0.5))
     
-    #adjust for weekday/weekend effect
-    if (input$adjust_time =='Overall') {
-        leaflet_plt_df <- citi_bike_pre_covid_df %>% 
-                            group_by(station_id) %>%
-                            summarise(total_start_count = sum(startcount),
-                                      total_end_count = sum(endcount),
-                                      total_day_diff = sum(day_diff),
-                                      total_diff_percentage = sum(day_diff)/sum(startcount),
-                            ) %>% left_join(citi_bike_station_info,by='station_id')
-    } else {
-        leaflet_plt_df <- citi_bike_pre_covid_df %>% 
-                            filter(weekend_or_weekday == input$adjust_time) %>%
-                            group_by(station_id) %>%
-                                summarise(total_start_count = sum(startcount),
-                                          total_end_count = sum(endcount),
-                                          total_day_diff = sum(day_diff),
-                                          total_diff_percentage = sum(day_diff)/sum(startcount),
-                                ) %>% left_join(citi_bike_station_info,by='station_id')
-                            } 
-
-        
-    map_2019 <- leaflet_plt_df %>%
-         leaflet(options = leafletOptions(minZoom = 11, maxZoom = 13)) %>%
-         addTiles() %>%
-         addProviderTiles("CartoDB.Positron",
-                          options = providerTileOptions(noWrap = TRUE)) %>%
-         setView(-73.9834,40.7504,zoom = 12)
-     
-     if (input$adjust_score == 'start_cnt') {
-         map_2019 %>%
-             addHeatmap(
-                        lng=~station_longitude,
-                        lat=~station_latitude,
-                        intensity=~total_start_count,
-                        max=4000,
-                        radius=8,
-                        blur=10)
-     }else if (input$adjust_score == 'end_cnt') {
-         map_2019 %>%
-             addHeatmap(
-                        lng=~station_longitude,
-                        lat=~station_latitude,
-                        intensity=~total_end_count,
-                        max=4000,
-                        radius=8,
-                        blur=10)
-     } else if (input$adjust_score == 'day_diff_absolute'){
-         map_2019 %>%
-             addHeatmap(
-                        lng=~station_longitude,
-                        lat=~station_latitude,
-                        intensity=~total_day_diff,
-                        max=50,
-                        radius=8,
-                        blur=10)
-         
-     }else if (input$adjust_score == 'day_diff_percentage'){
-         map_2019 %>%
-             addHeatmap(
-                        lng=~station_longitude,
-                        lat=~station_latitude,
-                        intensity=~total_diff_percentage,#change to total day diff percentage
-                        max=0.1,
-                        radius=8,
-                        blur=10)
-         
-     }
-     }) #left map plot
+  })
+  
+  output$hist <- renderPlot({
+    df_filtered = df_vio %>% 
+      filter(Boro == input$area) %>% 
+      pivot_longer(cols = "Family Felony Assaults":"Rape", names_to = "crime_type", values_to = "count")
     
-    output$right_map <- renderLeaflet({
-        #adjust for weekday/weekend effect
-        if (input$adjust_time =='Overall') {
-            leaflet_plt_df <- citi_bike_covid_df %>% 
-                group_by(station_id) %>%
-                summarise(total_start_count = sum(startcount),
-                          total_end_count = sum(endcount),
-                          total_day_diff = sum(day_diff),
-                          total_diff_percentage = sum(day_diff)/sum(startcount),
-                ) %>% left_join(citi_bike_station_info,by='station_id')
-        } else {
-            leaflet_plt_df <- citi_bike_covid_df %>% 
-                filter(weekend_or_weekday == input$adjust_time) %>%
-                group_by(station_id) %>%
-                summarise(total_start_count = sum(startcount),
-                          total_end_count = sum(endcount),
-                          total_day_diff = sum(day_diff),
-                          total_diff_percentage = sum(day_diff)/sum(startcount),
-                ) %>% left_join(citi_bike_station_info,by='station_id')
-        } 
-        #initial the map to plot on
-        map_2020 <- leaflet_plt_df %>%
-            leaflet(options = leafletOptions(minZoom = 11, maxZoom = 13)) %>%
-            addTiles() %>%
-            addProviderTiles("CartoDB.Positron",
-                             options = providerTileOptions(noWrap = TRUE)) %>%
-            setView(-73.9834,40.7504,zoom = 12) 
-        
-        if (input$adjust_score == 'start_cnt') {
-            map_2020 %>%
-                addHeatmap(
-                           lng=~station_longitude,
-                           lat=~station_latitude,
-                            intensity=~total_start_count, #change to total start count
-                            max=4000,
-                            radius=8,
-                           blur=10)
-        }else if (input$adjust_score == 'end_cnt') {
-            map_2020 %>%
-                addHeatmap(
-                           lng=~station_longitude,
-                           lat=~station_latitude,
-                           intensity=~total_end_count,#change to total end count
-                           max=4000,
-                           radius=8,
-                           blur=10)
-        } else if (input$adjust_score == 'day_diff_absolute'){
-            map_2020 %>%
-                addHeatmap(
-                           lng=~station_longitude,
-                           lat=~station_latitude,
-                           intensity=~total_day_diff,#change to total day diff
-                           max=50,
-                           radius=8,
-                           blur=10)
-            
-        }else if (input$adjust_score == 'day_diff_percentage'){
-            map_2020 %>%
-                addHeatmap(
-                           lng=~station_longitude,
-                           lat=~station_latitude,
-                           intensity=~total_diff_percentage,#change to total day diff percentage
-                           max=0.1,
-                           radius=8,
-                           blur=10)
-            
-        }
-        
-    }) #right map plot
+    ggplot(df_filtered, aes(x = Year, y = count, fill = crime_type)) +
+      geom_bar(stat = "identity", position = "dodge") +
+      scale_fill_manual(values = c("goldenrod1", "darkolivegreen3", "lightskyblue1", "sienna1")) +
+      ggtitle(paste0("Violence in ", input$area)) +
+      labs(x = "Year", y = "Count", fill = "Crime Type") +
+      theme(plot.title = element_text(hjust = 0.5))
+  })
+  
+  output$table = renderDataTable({
+    
+  })
+}
 
-})
+
+# Run the application 
+shinyApp(ui = ui, server = server)
 
 
--- app/ui.R
+++ app/ui.R
@@ -1,74 +1,181 @@
 
-if (!require("shiny")) {
-  install.packages("shiny")
-  library(shiny)
-}
-if (!require("shinyWidgets")) {
-  install.packages("shinyWidgets")
-  library(shinyWidgets)
-}
-if (!require("shinythemes")) {
-  install.packages("shinythemes")
-  library(shinythemes)
-}
-if (!require("leaflet")) {
-  install.packages("leaflet")
-  library(leaflet)
-}
-if (!require("leaflet.extras")) {
-  install.packages("leaflet.extras")
-  library(leaflet.extras)
-}
-
-# Define UI for application that draws a histogram
-shinyUI(
-    navbarPage(strong("Citi Bike Study",style="color: white;"), 
-               theme=shinytheme("cerulean"), # select your themes https://rstudio.github.io/shinythemes/
-#------------------------------- tab panel - Maps ---------------------------------
-                tabPanel("Maps",
-                         icon = icon("map-marker-alt"), #choose the icon for
-                         div(class = 'outer',
-                        # side by side plots
-                        fluidRow(
-                                splitLayout(cellWidths = c("50%", "50%"), 
-                                             leafletOutput("left_map",width="100%",height=1200),
-                                             leafletOutput("right_map",width="100%",height=1200))),
-                        #control panel on the left
-                        absolutePanel(id = "control", class = "panel panel-default", fixed = TRUE, draggable = TRUE,
-                                      top = 200, left = 50, right = "auto", bottom = "auto", width = 250, height = "auto",
-                                      tags$h4('Citi Bike Activity Comparison'), 
-                                      tags$br(),
-                                      tags$h5('Pre-covid(Left) Right(Right)'), 
-                                      prettyRadioButtons(
-                                                      inputId = "adjust_score",
-                                                      label = "Score List:", 
-                                                      choices = c("start_cnt", 
-                                                                  "end_cnt", 
-                                                                  "day_diff_absolute",
-                                                                  "day_diff_percentage"),
-                                                      inline = TRUE, 
-                                                      status = "danger",
-                                                      fill = TRUE
-                                                        ),
-                                      awesomeRadio("adjust_time", 
-                                                   label="Time",
-                                                    choices =c("Overall",
-                                                               "Weekday", 
-                                                               "Weekend"), 
-                                                    selected = "Overall",
-                                                    status = "warning"),
-                                      # selectInput('adjust_weather',
-                                      #             label = 'Adjust for Weather',
-                                      #             choices = c('Yes','No'), 
-                                      #             selected = 'Yes'
-                                      #             ),
-                                      style = "opacity: 0.80"
-                                      
-                                ), #Panel Control - Closing
-                            ) #Maps - Div closing
-                        ) #tabPanel maps closing
-   
 
 
-    ) #navbarPage closing  
-) #Shiny UI closing    
+library(dplyr)
+library(tidyr)
+library(scales)
+library(ggplot2)
+library(readr)
+
+#########################################
+#########################################
+#########################################
+#                                      ##
+#                                      ##
+#          ┌─┐       ┌─┐               ##
+#       ┌──┘ ┴───────┘ ┴──┐            ##
+#       │                 │            ##
+#       │       ───       │            ##
+#       │  ─┬┘       └┬─  │            ##
+#       │                 │            ##
+#       │       ─┴─       │            ##
+#       │                 │            ##
+#       └───┐         ┌───┘            ##
+#           │         │                ##
+#           │         │                ##
+#           │         │                ##
+#           │         └──────────────┐ ##
+#           │                        │ ##
+#           │                        ├─┐#
+#           │                        ┌─┘#    
+#           │                        │ ##
+#           └─┐  ┐  ┌───────┬──┐  ┌──┘ ##        
+#             │ ─┤ ─┤       │ ─┤ ─┤    ##     
+#             └──┴──┘       └──┴──┘    ##
+#                 神兽保佑             ##   
+#                代码无BUG!            ##  
+#########################################
+
+# Create the data frame of restaurant inspection
+# Keep only one record of each restaurant and drop NA rows
+restaurant <- read.csv("DOHMH_New_York_City_Restaurant_Inspection_Results.csv", header=T, sep=",")
+res_new <- restaurant %>% filter(INSPECTION.DATE>'01/01/1900' & BORO!='0' & BORO!='210' & GRADE!='') %>% group_by(DBA)
+df_res = res_new[, c('CAMIS', 'GRADE', 'BORO')]
+df_res = na.omit(df_res)
+df_res
+
+# Let grades other than A, B, and C be OTHER
+df_res$grade = ifelse((df_res$GRADE=='A')|(df_res$GRADE=='B')|(df_res$GRADE=='C'), df_res$GRADE, 'OTHER')
+class(df_res)
+counts = table(df_res$BORO, df_res$grade)
+total = rowSums(counts)
+A_amounts = counts[, "A"]
+ratios = A_amounts/total
+
+# Create a new data frame of counts and ratios
+boro = c('Bronx', 'Brooklyn', 'Manhattan', 'Queens', 'Staten Island')
+df_counts = as.data.frame.matrix(counts)
+df_counts$Ratios = ratios
+df_counts$Borough = boro
+df_counts
+
+
+
+#data visilization
+# Create the bar plot
+
+# Add a line and points for the portion of A
+
+
+df_counts
+#export dataset
+write.csv(df_res, "df_res.csv", row.names=FALSE)
+write.csv(df_counts, "df_counts.csv", row.names=FALSE)
+
+
+ggplot() + geom_col(aes(x = as.factor(BORO), y = 1, fill = grade), data = df_res, group = 1, position = "stack")
++ geom_line(aes(x = as.factor(df_counts$Borough), y = 39000*df_counts$Ratios, group = 1), color = "red",lwd=1)+ 
+  geom_point(aes(x = as.factor(df_counts$Borough), y = 39000*df_counts$Ratios), color = "black") +
+  scale_fill_manual(values=c("OTHER"='#E0A96D', "A"='#DDC3A5', "B"='gray24', "C"='seashell1')) +
+  labs(title= "Restaurants Grade Distribution", x = "Borough", y = "Counts") +
+  guides(fill=guide_legend(title="GRADE")) +
+  scale_y_continuous(sec.axis=sec_axis(~.*1/400, name="Ratios of Grade A")) +
+  theme(plot.title = element_text(hjust = 0.5))
+
+###traffic
+traffic <- read.csv("Automated_Traffic_Volume_Counts.csv", header=T, sep=",")
+traffic$time <- with(traffic, ISOdatetime(Yr, M, D, HH, MM, 0L))
+
+##group by different borough and sum up the volumes
+mantot <- traffic %>% filter(Boro=='Manhattan') %>% arrange(time) %>% group_by(time) %>% summarise(total_vol=sum(Vol),.groups = 'drop')
+brotot <- traffic %>% filter(Boro=='Brooklyn') %>% arrange(time) %>% group_by(time) %>% summarise(total_vol=sum(Vol),.groups = 'drop')
+qnstot <- traffic %>% filter(Boro=='Queens') %>% arrange(time) %>% group_by(time) %>% summarise(total_vol=sum(Vol),.groups = 'drop')
+brxtot <- traffic %>% filter(Boro=='Bronx') %>% arrange(time) %>% group_by(time) %>% summarise(total_vol=sum(Vol),.groups = 'drop')
+sitot <- traffic %>% filter(Boro=='Staten Island') %>% arrange(time) %>% group_by(time) %>% summarise(total_vol=sum(Vol),.groups = 'drop')
+
+#outer join dataframes
+m1 <- merge(x=mantot,y=brotot,by="time", all=TRUE)
+colnames(m1)[2] <- 'man'
+colnames(m1)[3] <- 'bro'
+m2 <- merge(x=m1,y=brxtot,by="time", all=TRUE)
+colnames(m2)[4] <- 'brx'
+m3 <- merge(x=m2,y=qnstot,by="time", all=TRUE)
+colnames(m3)[5] <- 'qns'
+m4 <- merge(x=m3,y=sitot,by="time", all=TRUE)
+colnames(m4)[6] <- 'si'
+m4[is.na(m4)] <- 0
+
+#set a proper time period to avoid too much missing data
+m4 <- m4[m4$time>'2019-09-10 00:00:00'&m4$time<'2019-11-25 00:00:00',]
+
+#data visualization
+x <- m4$time
+
+plot(x, m4$man, type="o", col="cornflowerblue", lwd=3, pch="o", ylab="Traffic Volume Counts",xlab='Month',lty=1, main='Daily Traffic Volume Counts from 7am to 10am')
+
+points(x, m4$bro, col="firebrick2", lwd=3, pch="*")
+lines(x, m4$bro, col="firebrick2", lwd=3,lty=1)
+
+points(x, m4$brx, col="burlywood1", lwd=3,pch="+")
+lines(x, m4$brx, col="burlywood1", lwd=3, lty=1)
+
+points(x, m4$qns, col="blueviolet", lwd=3,pch=5)
+lines(x, m4$qns, col="blueviolet", lwd=3, lty=1)
+
+points(x, m4$si, col="darkseagreen1", lwd=3,pch=7)
+lines(x, m4$si, col="darkseagreen1", lwd=3, lty=1)
+
+legend('topleft',legend=c("Manhattan","Brooklyn","Bronx",'Queens','Staten Island'), col=c("cornflowerblue","firebrick2","burlywood1",'blueviolet','darkseagreen1'),
+       lwd=3,pch=c(1,8,3,5,7),cex=1,lty=1,ncol=1)
+
+write.csv(m4, "morningtraffic.csv", row.names=FALSE)
+
+
+traffic2 <- read.csv("Automated_Traffic_Volume_Counts2.csv", header=T, sep=",")
+traffic2$time <- with(traffic2, ISOdatetime(Yr, M, D, HH, MM, 0L))
+##group by different borough and sum up the volumes
+man2 <- traffic2 %>% filter(Boro=='Manhattan') %>% arrange(time) %>% group_by(time) %>% summarise(total_vol=sum(Vol),.groups = 'drop')
+bro2 <- traffic2 %>% filter(Boro=='Brooklyn') %>% arrange(time) %>% group_by(time) %>% summarise(total_vol=sum(Vol),.groups = 'drop')
+qns2 <- traffic2 %>% filter(Boro=='Queens') %>% arrange(time) %>% group_by(time) %>% summarise(total_vol=sum(Vol),.groups = 'drop')
+brx2 <- traffic2 %>% filter(Boro=='Bronx') %>% arrange(time) %>% group_by(time) %>% summarise(total_vol=sum(Vol),.groups = 'drop')
+si2 <- traffic2 %>% filter(Boro=='Staten Island') %>% arrange(time) %>% group_by(time) %>% summarise(total_vol=sum(Vol),.groups = 'drop')
+
+#outer join dataframes
+m21 <- merge(x=man2,y=bro2,by="time", all=TRUE)
+colnames(m21)[2] <- 'man'
+colnames(m21)[3] <- 'bro'
+m22 <- merge(x=m21,y=brx2,by="time", all=TRUE)
+colnames(m22)[4] <- 'brx'
+m23 <- merge(x=m22,y=qns2,by="time", all=TRUE)
+colnames(m23)[5] <- 'qns'
+m24 <- merge(x=m23,y=si2,by="time", all=TRUE)
+colnames(m24)[6] <- 'si'
+m24[is.na(m24)] <- 0
+
+#set a proper time period to avoid too much missing data
+m24 <- m24[m24$time>'2019-09-10 00:00:00'&m24$time<'2019-11-25 00:00:00',]
+
+#data visualization
+x2 <- m24$time
+
+plot(x2, m24$man, type="o", col="cornflowerblue", pch="o", ylab="Counts",xlab='Month', lwd=3,lty=1,main='Daily Traffic Volume Counts from 5pm to 8pm')
+
+points(x2, m24$bro, col='firebrick2', lwd=3, pch="*")
+lines(x2, m24$bro, col='firebrick2', lwd=3,lty=1)
+
+points(x2, m24$brx, col="burlywood1", lwd=3,pch="+")
+lines(x2, m24$brx, col="burlywood1", lwd=3, lty=1)
+
+points(x2, m24$qns, col="blueviolet", lwd=3,pch=5)
+lines(x2, m24$qns, col="blueviolet", lwd=3, lty=1)
+
+points(x2, m24$si, col="darkseagreen1", lwd=3,pch=7)
+lines(x2, m24$si, col="darkseagreen1", lwd=3, lty=1)
+
+legend('topleft',legend=c("Manhattan","Brooklyn","Bronx",'Queens','Staten Island'), col=c("cornflowerblue","firebrick2","burlywood1",'blueviolet','darkseagreen1'),
+       lwd=3,pch=c(1,8,3,5,7),cex=1,lty=1,ncol=1)
+
+write.csv(m24, "eveningtraffic.csv", row.names=FALSE)
+
+
+########################################
